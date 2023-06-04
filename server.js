2eb3fc43eda954bb8a4d7e5e3c498836127013bb
Jeffrey Vicente <administrator@Jeffreys-MacBook-Air.local>
2023-06-04 15:59:40 +0100

    Added fuction to post notes to the application.

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-
-
 const PORT = 3001;
-
 const app = express();
+
 
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
@@ -20,18 +18,38 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/index.html"))
 });
 
-// GET request for upvotes
+
 app.get("/api/notes", (req, res) => {
-    fs.readFile(path.join(__dirname, "./db/db.json1"), "utf8", (error,notes) => {
+    fs.readFile(path.join(__dirname, "./db/db.json"), "utf8", (error,data) => {
         if (error) {
             return console.log(error)
         }
-        res.json(JSON.parse(notes))
+        res.json(JSON.parse(data))
     })
   });
 
-  
+  app.post('/api/notes', (req, res) => {
+    fs.readFile(path.join(__dirname, "./db/db.json"), "utf8", (error,data) => {
+        if (error){
+            return console.log(error);
+        }
+        var notes = JSON.parse(data);
+        let userNote = req.body;
+        userNote.id = Math.floor(Math.random() * 5000);
+        notes.push(userNote);
+        fs.writeFile('./db/db.json', JSON.stringify(notes), (err, data) => {
+        res.json(userNote);
+    });
+    }); 
+  });
   
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
 });
+
+
+
+
+
+
+  
